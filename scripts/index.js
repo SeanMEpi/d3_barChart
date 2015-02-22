b908fc1f2b2ed3864ec55d4ec150b99a45dfad0d
Sean Morrison <seanmorrison112358@gmail.com>
2015-02-21 16:15:48 -0800

    add update to bar chart; animated update as new items are added

--- scripts/index.js
+++ scripts/index.js
@@ -1,123 +1,121 @@
 //Width and height
-var w = 500;
-var h = 300;
-var padding = 30;
+var w = 600;
+var h = 250;
 
-//Dynamic, random dataset
-var dataset = [];
-var numDataPoints = 50;
-var maxRange = Math.random() * 1000;
-for (var i = 0; i < numDataPoints; i++) {
-  var newNumber1 = Math.floor(Math.random() * maxRange);
-  var newNumber2 = Math.floor(Math.random() * maxRange);
-  dataset.push([newNumber1, newNumber2]);
-}
+var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+  11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
 
-//Create scale functions
-var xScale = d3.scale.linear()
-  .domain([0, d3.max(dataset, function(d) { return d[0]; })])
-  .range([padding, w - padding * 2]);
+var xScale = d3.scale.ordinal()
+  .domain(d3.range(dataset.length))
+  .rangeRoundBands([0, w], 0.05);
 var yScale = d3.scale.linear()
-  .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-  .range([h - padding, padding]);
-
-//Define X axis
-var xAxis = d3.svg.axis()
-  .scale(xScale)
-  .orient("bottom")
-  .ticks(5);
-//Define Y axis
-var yAxis = d3.svg.axis()
-  .scale(yScale)
-  .orient("left")
-  .ticks(5);
+  .domain([0, d3.max(dataset)])
+  .range([0, h]);
 
 //Create SVG element
 var svg = d3.select("body")
   .append("svg")
   .attr("width", w)
   .attr("height", h);
-
-//Define clipping path (so pixels that land on edge won't display outside the graph)
-  svg.append("clipPath")
-  .attr("id", "chart-area")
-  .append("rect")
-  .attr("x", padding)
-  .attr("y", padding)
-  .attr("width", w - padding * 3)
-  .attr("height", h - padding * 2);
-
-//Create circles
-  svg.append("g")
-  .attr("id", "circles")
-  .attr("clip-path", "url(#chart-area)")
-  .selectAll("circle")
+//Create bars
+svg.selectAll("rect")
   .data(dataset)
   .enter()
-  .append("circle")
-  .attr("cx", function(d) {
-    return xScale(d[0]);
+  .append("rect")
+  .attr("x", function(d, i) {
+    return xScale(i);
   })
-  .attr("cy", function(d) {
-    return yScale(d[1]);
+  .attr("y", function(d) {
+    return h - yScale(d);
   })
-  .attr("r", 2);
-
-//Create X axis
-svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0," + (h - padding) + ")")
-  .call(xAxis);
-
-//Create Y axis
-svg.append("g")
-  .attr("class", "y axis")
-  .attr("transform", "translate(" + padding + ",0)")
-  .call(yAxis);
-
+  .attr("width", xScale.rangeBand())
+  .attr("height", function(d) {
+    return yScale(d);
+  })
+  .attr("fill", function(d) {
+    return "rgb(0, 0, " + (d * 10) + ")";
+  });
+//Create labels
+svg.selectAll("text")
+  .data(dataset)
+  .enter()
+  .append("text")
+  .text(function(d) {
+    return d;
+  })
+  .attr("text-anchor", "middle")
+  .attr("x", function(d, i) {
+    return xScale(i) + xScale.rangeBand() / 2;
+  })
+  .attr("y", function(d) {
+    return h - yScale(d) + 14;
+  })
+  .attr("font-family", "sans-serif")
+  .attr("font-size", "11px")
+  .attr("fill", "white");
 //On click, update with new data
 d3.select("p")
   .on("click", function() {
-    //New values for dataset
-    var numValues = dataset.length;
-    var maxRange = Math.random() * 1000;
-    dataset = [];
-    for (var i = 0; i < numValues; i++) {
-      var newNumber1 = Math.floor(Math.random() * maxRange);
-      var newNumber2 = Math.floor(Math.random() * maxRange);
-      dataset.push([newNumber1, newNumber2]);
-    }
+    //Add one new value to dataset
+    var maxValue = 25;
+    var newNumber = Math.floor(Math.random() * maxValue); //New random integer (0-24)
+    dataset.push(newNumber);                //Add new number to array
+
     //Update scale domains
-    xScale.domain([0, d3.max(dataset, function(d) { return d[0]; })]);
-    yScale.domain([0, d3.max(dataset, function(d) { return d[1]; })]);
-    //Update all circles
-    svg.selectAll("circle")
+    xScale.domain(d3.range(dataset.length));  //Recalibrate the x scale domain, given the new length of dataset
+    yScale.domain([0, d3.max(dataset)]);    //Recalibrate the y scale domain, given the new max value in dataset
+    //Select…
+    var bars = svg.selectAll("rect")      //Select all bars
+    .data(dataset);             //Re-bind data to existing bars, return the 'update' selection
+                        //'bars' is now the update selection
+
+    //Enter…
+    bars.enter()                //References the enter selection (a subset of the update selection)
+      .append("rect")             //Creates a new rect
+      .attr("x", w)             //Sets the initial x position of the rect beyond the far right edge of the SVG
+      .attr("y", function(d) {        //Sets the y value, based on the updated yScale
+        return h - yScale(d);
+      })
+      .attr("width", xScale.rangeBand())    //Sets the width value, based on the updated xScale
+      .attr("height", function(d) {     //Sets the height value, based on the updated yScale
+        return yScale(d);
+      })
+      .attr("fill", function(d) {       //Sets the fill value
+        return "rgb(0, 0, " + (d * 10) + ")";
+      });
+    //Update…
+    bars.transition()             //Initiate a transition on all elements in the update selection (all rects)
+      .duration(500)
+      .attr("x", function(d, i) {       //Set new x position, based on the updated xScale
+        return xScale(i);
+      })
+      .attr("y", function(d) {        //Set new y position, based on the updated yScale
+        return h - yScale(d);
+      })
+      .attr("width", xScale.rangeBand())    //Set new width value, based on the updated xScale
+      .attr("height", function(d) {     //Set new height value, based on the updated yScale
+        return yScale(d);
+      });
+    //Update labels
+    svg.selectAll("text").remove();
+    svg.selectAll("text")
       .data(dataset)
+      .enter()
+      .append("text")
       .transition()
-      .duration(1000)
-      .each("start", function() {
-        d3.select(this)
-        .attr("fill", "blue")
-        .attr("r", 3);
+      .duration(500)
+      .text(function(d) {
+        return d;
       })
-      .attr("cx", function(d) {
-        return xScale(d[0]);
+      .attr("text-anchor", "middle")
+      .attr("x", function(d, i) {
+        return xScale(i) + xScale.rangeBand() / 2;
       })
-      .attr("cy", function(d) {
-        return yScale(d[1]);
+      .attr("y", function(d) {
+        return h - yScale(d) + 14;
       })
-      .transition(1000)
-      .duration(1000)
-      .attr("fill", "black")
-      .attr("r", 2);
-    //Update x-axis
-    svg.select(".x.axis")
-      .transition()
-      .duration(1000)
-      .call(xAxis);
-    //Update y-axis
-    svg.select(".y.axis")
-      .transition()
-      .duration(1000)
-      .call(yAxis);
-  });
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "11px")
+      .attr("fill", "white");
+});
+
